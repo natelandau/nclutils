0750d6addf7a9c7c172a25e1e7e1d6edb364c84d
Nathaniel Landau <nate@natelandau.com>
2026-05-08 15:43:38 -0400

    refactor(ask): rename nclutils.questions to nclutils.ask

--- src/nclutils/ask/__init__.py
+++ src/nclutils/ask/__init__.py
@@ -1,5 +1,5 @@
 """Question utilities."""
 
-from .questions import choose_multiple_from_list, choose_one_from_list
+from .ask import choose_multiple_from_list, choose_one_from_list
 
 __all__ = ["choose_multiple_from_list", "choose_one_from_list"]
